8dbb402ad9c38a2eec2ecc4b5161fb73a28f76c4
fai-sal-786 <154748475+fai-sal-786@users.noreply.github.com>
2024-08-31 03:49:08 -0700

    event planner

---
+++ event planner 1/utils/utils.js
@@ -0,0 +1,70 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  collection,
+  addDoc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+  query,
+  where,
+  deleteDoc
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_Mf8dUWoAViA7LsS86wS9jv8WnlE9iPI",
+  authDomain: "complete-project-bc854.firebaseapp.com",
+  projectId: "complete-project-bc854",
+  storageBucket: "complete-project-bc854.appspot.com",
+  messagingSenderId: "708274302267",
+  appId: "1:708274302267:web:20b883fec1d266ef2d9b1e",
+  measurementId: "G-5VSB1RE7XP"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+export {
+  auth,
+  db,
+  storage,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  doc,
+  setDoc,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  signOut,
+  getDoc,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+  query,
+  where,
+  deleteDoc
+};
